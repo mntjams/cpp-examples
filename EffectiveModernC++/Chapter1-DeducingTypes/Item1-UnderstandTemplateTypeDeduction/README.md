acae394f9c8e956104b3a29610dafff1a960bcae
mntjams <matouszatloukal@seznam.cz>
2026-06-22 07:59:39 +0200

    Init + first example

---
+++ EffectiveModernC++/Chapter1-DeducingTypes/Item1-UnderstandTemplateTypeDeduction/README.md
@@ -0,0 +1,15 @@
+# Understand template type deduction
+
+Pseudocode of a function template and its call:
+```C++
+template <typename T>
+void f(ParamType param);
+
+f(expr);
+```
+
+Type deduction is (expectedly) based on `expr` type but also on `ParamType`.
+There are three cases:
+1. `ParamType` is a pointer or a reference, but not universal reference.
+2. `ParamType` is a universal reference.
+3. `ParamType` is neither a pointer nor a reference.
---
+++ EffectiveModernC++/Chapter1-DeducingTypes/Item1-UnderstandTemplateTypeDeduction/pointer-or-reference.cpp
@@ -0,0 +1,45 @@
+/*
+ * 1. If expr's type is a reference, ignore the reference part.
+ * 2. Then pattern-match expr's type against ParamType to determine T.
+ */
+
+template <typename T>
+class TypeDeducer;
+
+template <typename T>
+void f(T& p) {};
+
+template <typename T>
+void cf(const T& p) {};
+
+template <typename T>
+void g(T* p) {};
+
+int main() {
+    int x = 1;
+    const int cx = x;
+    const int& crx = x;
+
+    f(x);    // ParamType is int&, T is int
+    f(cx);   // ParamType is const int&, T is const int
+    f(crx);  // ParamType is const int&, T is const int
+
+    // Now const is part of the ParamType, therefore T becomes just int
+    cf(x);    // ParamType is const int&, T is int
+    cf(cx);   // ParamType is const int&, T is int
+    cf(crx);  // ParamType is const int&, T is int
+
+    // With pointers, it works identically
+
+    const int* cpx = &x;
+
+    // "Taking address of" is not implicit as "taking reference of"
+    // is, so we have to do it manually with &.
+    g(&x);   // ParamType is int*, T is int
+    g(&cx);  // ParamType is const int*, T is int
+
+    g(cpx);  // ParamType is const int*, T is int
+
+    // With ParamType const int*, it works as expected
+    return 0;
+};
